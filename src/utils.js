d9789f19991c7f43e8540f1c44cba3744dcf04fa
PJMantoss <joelptoss@gmail.com>
2021-09-07 13:49:45 +0100

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,11 @@
 export const formatData = data => {
     let finalData = {
         labels: [],
-        datasets: []
+        datasets: [
+            {
+                label: "price",
+                data: []
+            }
+        ]
     }
 }
