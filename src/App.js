c39b34fcece906073ccdc83ce3160ab067af37d2
PJMantoss <joelptoss@gmail.com>
2021-09-07 09:51:48 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import './App.css';
 function App() {
   const [currencies, setCurrencies] = useState([]);
   const [pair, setPair] = useState("");
-  
+  const [price, setPrice] = useState("0.00");
+  const [pastData, setPastData] = useState({});
+
   return (
     <div className="App">
       <h1>Hello</h1>
