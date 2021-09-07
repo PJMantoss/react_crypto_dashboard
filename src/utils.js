9a8335abae044575cb8622ce266e37974b88514e
PJMantoss <joelptoss@gmail.com>
2021-09-07 13:50:59 +0100

    update utils

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,10 @@ export const formatData = data => {
         datasets: [
             {
                 label: "price",
-                data: []
+                data: [],
+                backgroundColor: "rgb(255, 99, 132, 0.8)",
+                borderColor: "rgba(255, 99, 132, 0.2)",
+                fill: false
             }
         ]
     }
